033aadfdafd0eabe3b6f2f64cae8e17121ce0eb1
Bill Pace <billpace24@gmail.com>
2023-08-14 19:41:21 -0500

    create channel with basic usage demo

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "channel.h"
+
+#include <iostream>
+#include <thread>
+#include <sstream>
+
+using namespace mpsc;
+using namespace std;
+
+void send_task(const std::string &, mpsc::Transmitter<std::string>);
+void receive_task(mpsc::Receiver<std::string> &&);
+
+int main() {
+    auto tx_rx = open_channel<std::string>();
+    thread receiver_thread { receive_task, std::move(tx_rx.second) };
+
+    {
+        auto tx = std::move(tx_rx.first);
+        thread transmitter_threads[10];
+        for (size_t i = 0; i < 10; ++i) {
+            stringstream name{};
+            name << "Thread " << i;
+            transmitter_threads[i] = thread(send_task, name.str(), tx);
+        }
+
+        for (auto &thread: transmitter_threads) {
+            thread.join();
+        }
+    }
+    receiver_thread.join();
+}
+
+void send_task(const std::string & name, mpsc::Transmitter<std::string> tx) {
+    string message { name + " says hello!" };
+    tx.send(message);
+}
+
+void receive_task(mpsc::Receiver<std::string> && rx) {
+    bool channel_open { true };
+    while (channel_open) {
+        string msg {};
+        while (rx.try_receive(msg, channel_open)) {
+            cout << msg << endl;
+        }
+    }
+}
